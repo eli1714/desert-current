698d8c8f90d9a666abda72494c74b01c756205ac
Elias Fuentes <eliasafuentes@gmail.com>
2026-04-20 16:31:01 -0700

    Hide default page title on Elementor-built pages

--- wp-content/themes/desert-current/template-parts/content/content-page.php
+++ wp-content/themes/desert-current/template-parts/content/content-page.php
@@ -7,12 +7,15 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'entry entry-page' ); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header>
+	<?php $is_elementor_page = 'builder' === get_post_meta( get_the_ID(), '_elementor_edit_mode', true ); ?>
+
+	<?php if ( ! $is_elementor_page ) : ?>
+		<header class="entry-header">
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</header>
+	<?php endif; ?>
 
 	<div class="entry-content">
 		<?php the_content(); ?>
 	</div>
 </article>
-
